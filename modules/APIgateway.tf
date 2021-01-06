a5ea32b2bf707df8c34f7cb3cccb7d32ae7b09a7
Michael Campbell <michael.campbell@northwestern.edu>
2021-01-06 09:34:48 -0600

    trying to fix cors issue with api gateway

--- modules/APIgateway.tf
+++ modules/APIgateway.tf
@@ -56,9 +56,84 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.mrc053_WildRydes_lambdafunc.invoke_arn
 }
 
+# Allows OPTIONS method
+resource "aws_api_gateway_method" "options_method_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.mrc053_api.id
+  resource_id   = aws_api_gateway_resource.ride.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+#CORS response and parameters
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.mrc053_api.id
+  resource_id = aws_api_gateway_resource.ride.id
+  http_method = aws_api_gateway_method.api_method.http_method
+  status_code = "200"
+} 
+resource "aws_api_gateway_method_response" "method_response" {
+  rest_api_id   = aws_api_gateway_rest_api.mrc053_api.id
+  resource_id   = aws_api_gateway_resource.ride.id
+  http_method   = aws_api_gateway_method.api_method.http_method
+  status_code   = "200"
+} 
+
+# CORS support for mock integration
+resource "aws_api_gateway_integration" "integration_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.mrc053_api.id
+  resource_id   = aws_api_gateway_resource.ride.id
+  http_method   = aws_api_gateway_method.options_method_cors.http_method
+  type          = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+      { "statusCode": 200 }
+  EOF
+  }
+}
+resource "aws_api_gateway_method_response" "method_response_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.mrc053_api.id
+  resource_id   = aws_api_gateway_resource.ride.id
+  http_method   = aws_api_gateway_method.options_method_cors.http_method
+  status_code   = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.options_method_cors
+  ]
+}
+resource "aws_api_gateway_integration_response" "integration_response_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.mrc053_api.id
+  resource_id   = aws_api_gateway_resource.ride.id
+  http_method   = aws_api_gateway_method.options_method_cors.http_method
+  status_code   = "200"
+
+  depends_on = [
+    aws_api_gateway_integration.integration_cors
+  ]
+}
+
+# Gives permissions to API Gateway to invoke lambda
+resource "aws_lambda_permission" "mrc_api_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.mrc053_WildRydes_lambdafunc.name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = ""
+   
+} 
+
 # Deploys API to dev stage
 resource "aws_api_gateway_deployment" "deployment" {
     depends_on = [aws_api_gateway_integration.integration]
     rest_api_id = aws_api_gateway_rest_api.mrc053_api.id
     stage_name = "dev"
 }
+
+
